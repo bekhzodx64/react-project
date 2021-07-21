1f6be253bf23f04cab250d6f6005b4a09a76fb1e
bekhzod <mr.bekzod2018@gmail.com>
2021-07-22 00:29:21 +0500

    project complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './layout/Header.jsx';
+import { Footer } from './layout/Footer.jsx';
+import { Main } from './layout/Main.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
